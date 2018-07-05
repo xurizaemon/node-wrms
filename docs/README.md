b09b264d8acc0aacc7172ab39199129f825697e5
Chris Burgess <chrisburgess@catalyst.net.nz>
2018-07-06 10:37:30 +1200

    🐕 Document some WRMS usage by example

---
+++ docs/README.md
@@ -0,0 +1,12 @@
+# Documentation
+
+Living documentation?
+
+... OK, examples?
+
+Look, it's just a bunch of random things I did with WRMS, in a folder called "docs".
+
+## Examples
+
+* [`assign-tickets-as-child.js`](assign-tickets-as-child.js) - // Assign a bunch of WRs to a parent WR. The link types in WRMS have never seemed meaningful to me, so I generally use the default, "I" (for "implemented in").
+* [`show-hours-incl-children.js`](show-hours-incl-children.js) - Given that some tickets are child tickets (via "I") of a parent WR, find them and print their hours. WRMS should do this in the UI ...
---
+++ docs/show-hours-incl-children.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// Given that some tickets are child tickets (via "I") of a parent WR,
+// find them and print their hours.
+// WRMS should do this in the UI ...
+
+const WRMS = require('wrms')
+const creds = require('../.creds')
+const wrms = new WRMS(creds)
+
+let parent_id = 295131
+
+wrms.work_request.get(parent_id)
+  .then(res => {
+    let parent_wr = res.response;
+    console.log(`* WR#${parent_id} - ${parent_wr.hours.toFixed(2)}H (parent)`)
+    if (typeof parent_wr.children_by_type.I !== 'object') {
+      console.log('No child requests found.')
+    }
+    else {
+      for (let child_id of parent_wr.children_by_type.I) {
+        wrms.work_request.get(child_id)
+          .then((res) => {
+            let child_wr = res.response
+            console.log(`* WR#${child_id} - ${child_wr.hours.toFixed(2)}H`)
+          })
+      }
+    }
+  })
